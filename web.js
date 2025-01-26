a06cb116943ac07912bcd656ac66d8b08c410759
Ayyoobur-Rahman <Ayyoob7860@hotmail.com>
2025-01-26 19:41:25 +0000

    Add files via upload

---
+++ web.js
@@ -0,0 +1,80 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+
+
+ function changeImage() {
+    var img = document.getElementById('myImage');
+    img.src = 'https://i.kym-cdn.com/entries/icons/original/000/044/399/pgcover.jpg'; // Change to the new image source
+    img.style.height = "500px";
+    img.style.width = "600px";
+}
+
+function resetImage() {
+    var img = document.getElementById('myImage');
+    img.src = 'https://th.bing.com/th/id/OIP.nFVuVBcE7sFrafJB27PGfAHaE1?w=248&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'; // Reset to the original image source
+    img.style.height = "300px";
+    img.style.width = "400px";
+}
+
+function changeImage2() {  //yamcha
+    var img = document.getElementById('myImage2');
+    img.src = 'https://sm.ign.com/ign_latam/screenshot/default/yamcha-muerto_mmxc.jpg'; // Change to the new image source
+    img.style.height = "500px";
+    img.style.width = "600px";
+}
+
+function resetImage2() {
+    var img = document.getElementById('myImage2');
+    img.src = 'https://th.bing.com/th/id/OIP.pH1zXwHOJTukLQCJ5ZnQxAHaFj?rs=1&pid=ImgDetMain'; // Reset to the original image source
+    img.style.height = "300px";
+    img.style.width = "400px";
+}
+
+
+
+
+
+
+
+function changeImage4() {//saiyans
+    var img = document.getElementById('myImage4');
+    img.src = 'https://i.pinimg.com/originals/1e/e0/55/1ee055b5c2ad6a140802c203e608079f.png'; // Change to the new image source
+    img.style.height = "500px";
+    img.style.width = "600px";
+}
+
+function resetImage4() {
+    var img = document.getElementById('myImage4');
+    img.src = 'https://th.bing.com/th/id/R.2bb1ba060963f42f5b8ed7b94ca088f1?rik=XFhf5GGs6Eiudg&riu=http%3a%2f%2fwww.xmwallpapers.com%2fwallpaper%2fanime%2fdragonball%2f2bb1ba060963f42f5b8ed7b94ca088f1.jpg&ehk=oBNDXKX%2f2dhg7nuHz2a%2fSvxAg%2bc%2bOVXiKlFy6w9aj8w%3d&risl=&pid=ImgRaw&r=0'; // Reset to the original image source
+    img.style.height = "300px";
+    img.style.width = "400px";
+}
+function changeImage5() {  //ss4 Gogeta
+    var img = document.getElementById('myImage5');
+    img.src = 'https://th.bing.com/th/id/R.5e9fe7f23fbc8fde4fc3b95a9f82224a?rik=9w1ai4up2lVBaw&riu=http%3a%2f%2forig02.deviantart.net%2f1471%2ff%2f2016%2f180%2fb%2f7%2fcabba_super_saiyan_gif_by_pikachustar93-da84lnb.gif&ehk=QozvIa0b822c4sllghChbMw%2bpSQ%2fuDFy7gVQobtJhTE%3d&risl=&pid=ImgRaw&r=0'; // Change to the new image source
+    img.style.height = "500px";
+    img.style.width = "600px";
+}
+
+function resetImage5() {
+    var img = document.getElementById('myImage5');
+    img.src = "https://th.bing.com/th/id/OIP.lCSxD7xyy0Lqo3M-gjEEhAHaNK?rs=1&pid=ImgDetMain" ; // Reset to the original image source
+    img.style.height = "300px";
+    img.style.width = "400px";
+}
+function changeImage6() {
+    var img = document.getElementById('myImage6');
+    img.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e130ef0a-cd00-4b66-ae02-b5a703ac6a9d/de5qi87-dbb71977-7d2f-4632-ad27-7ada21ab9726.png/v1/fill/w_730,h_1095,q_70,strp/son_goku_mui_vs_moro__dragon_ball_super_64__by_raphaeldslt_de5qi87-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTEyNSIsInBhdGgiOiJcL2ZcL2UxMzBlZjBhLWNkMDAtNGI2Ni1hZTAyLWI1YTcwM2FjNmE5ZFwvZGU1cWk4Ny1kYmI3MTk3Ny03ZDJmLTQ2MzItYWQyNy03YWRhMjFhYjk3MjYucG5nIiwid2lkdGgiOiI8PTc1MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.EbG_vP7ypXuZNNpYnA0oTl6GTaoPEu2rOj84ykwIqYs"; // Change to the new image source
+    img.style.height = "500px";
+    img.style.width = "600px";
+}
+
+function resetImage6() {
+    var img = document.getElementById('myImage6');
+    img.src = "https://wallpapercave.com/wp/wp8097993.jpg"; // Reset to the original image source
+    img.style.height = "300px";
+    img.style.width = "400px";
+}
